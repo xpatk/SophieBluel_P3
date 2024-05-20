23b6ee0615fb2515e5b3c3a8b557539afb04e215
xpat <patrycja.kruczynska@gmail.com>
2024-05-20 16:29:11 +0200

    separated the functions fetch and display and added event listeners to filter buttons

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,43 +1,79 @@
-// DOM container
+// DOM container for works
 const gallery = document.querySelector(".gallery");
+let works = [];
 
+// function fetch data from API
 async function fetchWorks() {
   try {
-    // fetch data from API
     const response = await fetch("http://localhost:5678/api/works");
-
     // if response is different than ok
     if (!response.ok) {
       throw new Error("Couldn't fetch data to display");
     }
-
-    const works = await response.json();
-
-    // create DOM elements
-    for (let i = 0; i < works.length; i++) {
-      const work = works[i];
-
-      // create image element
-      const imageElement = document.createElement("img");
-      imageElement.src = work.imageUrl;
-
-      //create title element
-      const titleElement = document.createElement("figcaption");
-      titleElement.innerText = work.title;
-
-      //create figure element
-      const figureElement = document.createElement("figure");
-
-      // attach elements to the page
-      figureElement.appendChild(imageElement);
-      figureElement.appendChild(titleElement);
-
-      gallery.appendChild(figureElement);
-    }
+    // if response is ok call function displayWorks
+    works = await response.json();
+    displayWorks(works);
   } catch (error) {
     console.error(error);
   }
 }
 
-// Call the function to fetch and display works
+// function display works on the page
+function displayWorks(worksToDisplay) {
+  gallery.innerHTML = "";
+
+  for (let i = 0; i < worksToDisplay.length; i++) {
+    const work = worksToDisplay[i];
+
+    // create image element
+    const imageElement = document.createElement("img");
+    imageElement.src = work.imageUrl;
+
+    //create title element
+    const titleElement = document.createElement("figcaption");
+    titleElement.innerText = work.title;
+
+    //create figure element
+    const figureElement = document.createElement("figure");
+
+    // attach elements to the page
+    figureElement.appendChild(imageElement);
+    figureElement.appendChild(titleElement);
+
+    gallery.appendChild(figureElement);
+  }
+}
+
+// Call the function to fetch and display all of the works to begin with
 fetchWorks();
+
+// button to display all works
+const buttonTous = document.querySelector(".btn-tous");
+buttonTous.addEventListener("click", function () {
+  displayWorks(works);
+});
+
+// button to display objects
+const buttonObjects = document.querySelector(".btn-objets");
+buttonObjects.addEventListener("click", function () {
+  const filteredWorks = works.filter((work) => work.category.name === "Objets");
+  displayWorks(filteredWorks);
+});
+
+// button to display apartments
+const buttonApartments = document.querySelector(".btn-appartements");
+buttonApartments.addEventListener("click", function () {
+  const filteredWorks = works.filter(
+    (work) => work.category.name === "Appartements"
+  );
+  displayWorks(filteredWorks);
+});
+
+// button to display hotels and restaurants
+const buttonHotels = document.querySelector(".btn-hotels-restaurants");
+buttonHotels.addEventListener("click", function () {
+  const filteredWorks = works.filter(
+    (work) => work.category.name === "Hotels & restaurants"
+  );
+  displayWorks(filteredWorks);
+});
