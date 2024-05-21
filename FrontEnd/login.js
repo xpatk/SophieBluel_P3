66252995b93e14f244c29ab15553a59be9c377b9
xpat <patrycja.kruczynska@gmail.com>
2024-05-21 23:29:07 +0200

    error handling in login js

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -3,6 +3,11 @@
  Logging in 
 
 *****************/
+
+// preparing error handling with DOM
+let errorElement = document.querySelector(".error");
+errorElement.style.display = "none";
+
 function listenerSubmit() {
   // add event listenet to the form
   const formLogin = document.querySelector(".form-login");
@@ -29,8 +34,13 @@ function listenerSubmit() {
 
       // errow handling
       if (!response.ok) {
+        errorElement.textContent = "Something went wrong.";
+        errorElement.style.display = "block";
         throw new Error(`Unable to fetch data: ${response.status}`);
       }
+
+      // Remove the error element if present
+      errorElement.style.display = "none"; // Hide the error element
 
       // if response is ok create a const token with the token from the server response
       const data = await response.json();
@@ -38,6 +48,9 @@ function listenerSubmit() {
       const token = data.jwt;
       // store the token in local storage
       window.localStorage.setItem("authToken", token);
+
+      // redirection to the main page
+      window.location.href = "./index.html";
       console.log("Success! Token:", token);
     } catch (error) {
       console.error("Error:", error);
